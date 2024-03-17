70c60d9a9b81cfd87fd8f4b774edcf0c7fa874cc
Julian <julianb0904@gmail.com>
2024-03-17 17:18:47 -0500

    First commit

---
+++ src/main/java/co/edu/uptc/P_projectSubjects/PProjectSubjectsApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uptc.P_projectSubjects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PProjectSubjectsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PProjectSubjectsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/uptc/P_projectSubjects/PProjectSubjectsApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uptc.P_projectSubjects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PProjectSubjectsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
